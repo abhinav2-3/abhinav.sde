bdd3133a365d4cb54329bda1e99c0a86970aa92b
abhinav2-3 <abhinavgtechnical@gmail.com>
2026-05-21 12:01:55 +0530

    Added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Analytics } from "@vercel/analytics/react";
+import { inject } from "@vercel/analytics";
+inject();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Analytics />
     <App />
   </StrictMode>,
 );
